a85a705712ab5431761f0cd38719cda058d106b3
Jason Won <jason.j.won@gmail.com>
2016-07-16 13:38:35 -0700

    add resets

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,12 @@
 import React from 'react'
-import { IndexLink, Link } from 'react-router'
+import { IndexLink } from 'react-router'
 import classes from './Header.scss'
 
 export const Header = () => (
   <div>
-    <h1>Project Homeless Connect</h1>
-    <IndexLink to='/' activeClassName={classes.activeRoute}>
-      Home
-    </IndexLink>
-    {' · '}
-    <Link to='/check-in' activeClassName={classes.activeRoute}>
-      Check In
-    </Link>
-    {' · '}
-    <Link to='/services' activeClassName={classes.activeRoute}>
-      Services
-    </Link>
+    <h1 className={classes.heading}>
+      <IndexLink to='/'>Project Homeless Connect</IndexLink>
+    </h1>
   </div>
 )
 
