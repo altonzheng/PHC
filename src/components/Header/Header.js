bdaf6126187864fc159096a8445a739936727a12
Jason Won <jason.j.won@gmail.com>
2016-07-16 16:29:17 -0700

    add helmet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import { IndexLink } from 'react-router'
 import classes from './Header.scss'
 
 export const Header = () => (
   <div>
+    <Helmet title="Project Homeless Connect" />
     <h1 className={classes.heading}>
-      <IndexLink to='/'>Project Homeless Connect</IndexLink>
+      <IndexLink to="/">Project Homeless Connect</IndexLink>
     </h1>
   </div>
 )
