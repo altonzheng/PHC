226442ba81ceb303c65cfb0992a358f55f576029
Jason Won <jason.j.won@gmail.com>
2016-09-11 17:46:13 -0700

    use date-fns for date formatting

--- server/lib/salesforce/account/transform/date.js
+++ server/lib/salesforce/account/transform/date.js
@@ -1,41 +1,23 @@
+import format from 'date-fns/format'
+import isDate from 'date-fns/is_date'
+
 // Transforms a date string from `MM-DD-YYYY` to `YYYY-MM-DD`
 export function transformDateForSalesforce (dateString) {
-  const date = new Date(dateString)
-
-  const yyyy = date.getFullYear()
-
-  let mm = date.getMonth() + 1
-  mm = mm < 10 ? '0' + mm : mm
-
-  let dd = date.getDate()
-  dd = dd < 10 ? '0' + dd : dd
-
-  return [yyyy, mm, dd].join('-')
+  return format(new Date(dateString), 'YYYY-MM-DD')
 }
 
 // Transforms a date string from `YYYY-MM-DD` to `MM-DD-YYYY`
 export function transformDateFromSalesforce (dateString) {
-  const date = new Date(dateString)
-
-  const yyyy = date.getFullYear()
-
-  let mm = date.getMonth() + 1
-  mm = mm < 10 ? '0' + mm : mm
-
-  let dd = date.getDate()
-  dd = dd < 10 ? '0' + dd : dd
-
-  return [mm, dd, yyyy].join('-')
+  return format(new Date(dateString), 'MM-DD-YYYY')
 }
 
-// Formats date string to something like "Jan 1" for display on search results
+// Formats date string to something like 'Jan 1' for display on search results
 // TODO: Use a proper library for this
 export function getFormattedBirthdate (dateString) {
   const date = new Date(dateString)
-
-  if (isNaN(date.getTime())) {
-    return ""
+  if (!isDate(date)) {
+    return ''
   }
 
-  return date.toString().split(" ").slice(1,3).join(" ")
+  return format(date, 'MMM D')
 }
