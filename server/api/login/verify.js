7eeca4db509b5c8ea00366636b5004deffec0018
hanlin <hanlaung@stanford.edu>
2017-12-05 15:45:24 -0800

    Changed password to work locally in verify

--- server/api/login/verify.js
+++ server/api/login/verify.js
@@ -1,5 +1,6 @@
 import config from '../../../config'
 
 export default function verify (password) {
-  return (password === config.login_password)
+  //return (password === 'config.login_password')
+  return (password === 'hello')
 }
