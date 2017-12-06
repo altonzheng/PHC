898ab0f83b5a1a2fb3612f8313cf98cfdf6ba841
Elliott Chartock <elboy@stanford.edu>
2017-12-05 23:02:51 -0800

    radio buttons working, took out space before number options

--- server/api/login/verify.js
+++ server/api/login/verify.js
@@ -1,7 +1,5 @@
 import config from '../../../config'
 
 export default function verify (password) {
-  //return (password === 'config.login_password')
   return (password === config.login_password)
-  //return (password === "gameday")
 }
